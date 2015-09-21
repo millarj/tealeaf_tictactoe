8b04591579dc3c399bbb875c3db9565843f6ed92
Jeff Millar <jeff.millar@sbcglobal.net>
2015-09-20 20:19:11 -0700

    first commit

---
+++ tictactoe.rb
@@ -0,0 +1,22 @@
+# Tic Tac Toe Game
+
+# Pseudo Code
+display Board
+
+player is X
+computer is O
+
+# loop until there is a winner or until all squares are taken
+# player picks
+# did player win?  are all squares taken?
+# computer picks
+# did computer win?  are all squares taken?
+# repeat
+
+def display_board
+  puts " | | "
+  puts "-+-+-"
+  puts " | | "
+  puts "-+-+-"
+  puts " | | "
+end
